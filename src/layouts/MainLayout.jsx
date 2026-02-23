197c69b63678e6af6066a259cb70fa04d6a0208a
Gihan Akalanka <gihan0325@gmail.com>
2026-02-23 22:07:02 +0530

    feat: implement responsive MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,53 @@
 
 
 function MainLayout() {
-    return (<>
-        <header>
-
+    return (<div className="min-h-screen bg-white dark:bg-slate-950 p-5 lg:p-10">
+        <header className="">
+            <h1 className="dark:text-white text-4xl">Welcome!</h1>
         </header>
-        <sidebar>
+        <aside className="w-64 hidden md:flex">
 
-        </sidebar>
-        <main>
+        </aside>
+        <main className="mt-10">
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                <div className="bg-slate-900 p-5 rounded-lg space-y-5">
+                    <h3 className="text-slate-50">Total Investment!</h3>
+
+                    <div className="space-y-5">
+                        <div className="flex justify-between">
+                            <span className="text-slate-50">Total Invested</span>
+                            <span className="text-slate-50 font-bold text-1xl">$1.00</span>
+                        </div>
+                        <div className="flex justify-between">
+                            <span className="text-slate-50">Current Value</span>
+                            <span className="text-slate-50 font-bold text-1xl">$1.00</span>
+                        </div>
+
+                        <div className="flex justify-between">
+                            <span className="text-slate-50">Total P/L ($ + %)</span>
+                            <div className="flex flex-col items-end space-y-1">
+                                <span className="text-slate-50 font-bold text-2xl">$1.00</span>
+                                <span class="text-slate-400">34.07%</span>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div className="bg-slate-900 p-5 rounded-lg">
+                    <h3 className="dark:text-white">Portfolio Allocation</h3>
+
+                </div>
+                <div className="bg-slate-900 p-5 rounded-lg">
+                    <h3 className="dark:text-white">Performance Overview</h3>
+                </div>
+            </div>
 
         </main>
-    </>);
+    </div>);
 }
 
 export default MainLayout;
