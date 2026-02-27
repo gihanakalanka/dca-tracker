2ad0a195fdc6f3804a0ba82be857699602107814
Gihan Akalanka <gihan0325@gmail.com>
2026-02-27 07:21:46 +0530

    Make menu stylish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
 
 function Header({ title }) {
+
     return (
-        <header className="">
+        <header className="flex justify-between item-center">
             <h1 className="dark:text-white text-4xl">{title}</h1>
-            <nav>
-                <NavLink to="/" className="text-slate-50">Dashbaord</NavLink>
-                <NavLink to="/portfolio" className="text-slate-50">Portfolio</NavLink>
+            <nav className="space-x-5">
+                <NavLink to="/"
+                    className={({ isActive }) => `hover:text-amber-400 ${isActive ? "text-amber-400" : "text-slate-50"}`}>
+                    Dashboard
+                </NavLink>
+                <NavLink to="/portfolio" className={({ isActive }) => `hover:text-amber-400 ${isActive ? "text-amber-400" : "text-slate-50"}`}>Portfolio</NavLink>
             </nav>
-        </header>
+        </header >
     );
 }
 
--- src/features/dashboard/DashboardPage.jsx
+++ src/features/dashboard/DashboardPage.jsx
@@ -2,37 +2,40 @@ import SummeryCard from "../../components/SummeryCard";
 
 function DashbaordPage() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div >
 
-            <SummeryCard title="Total Investment!">
-                <div className="flex justify-between">
-                    <span className="text-slate-50">Total Invested</span>
-                    <span className="text-slate-50 font-bold text-1xl">$1.00</span>
-                </div>
-
-                <div className="flex justify-between">
-                    <span className="text-slate-50">Current Value</span>
-                    <span className="text-slate-50 font-bold text-1xl">$1.00</span>
-                </div>
-
-                <div className="flex justify-between">
-                    <span className="text-slate-50">Total P/L ($ + %)</span>
-                    <div className="flex flex-col items-end space-y-1">
-                        <span className="text-slate-50 font-bold text-2xl">$1.00</span>
-                        <span className="text-slate-400">34.07%</span>
+            <section className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                <SummeryCard title="Total Investment!">
+                    <div className="flex justify-between">
+                        <span className="text-slate-50">Total Invested</span>
+                        <span className="text-slate-50 font-bold text-1xl">$1.00</span>
                     </div>
 
-                </div>
-            </SummeryCard>
+                    <div className="flex justify-between">
+                        <span className="text-slate-50">Current Value</span>
+                        <span className="text-slate-50 font-bold text-1xl">$1.00</span>
+                    </div>
 
-            <SummeryCard title="Portfolio Allocation">
+                    <div className="flex justify-between">
+                        <span className="text-slate-50">Total P/L ($ + %)</span>
+                        <div className="flex flex-col items-end space-y-1">
+                            <span className="text-slate-50 font-bold text-2xl">$1.00</span>
+                            <span className="text-slate-400">34.07%</span>
+                        </div>
 
-            </SummeryCard>
+                    </div>
+                </SummeryCard>
+
+                <SummeryCard title="Portfolio Allocation">
+
+                </SummeryCard>
 
 
-            <SummeryCard title="Performance Overview">
+                <SummeryCard title="Performance Overview">
 
-            </SummeryCard>
+                </SummeryCard>
+            </section>
+
         </div>
 
     );
